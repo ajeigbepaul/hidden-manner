2e2378bf7cda7f2d389fbc7f3ffce6f5cbfbd7cd
ajeigbepaul <pdave4krist@yahoo.com>
2025-07-01 09:13:19 +0100

    added the youtubeschema

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -5,5 +5,6 @@ import nugget from './nugget'
 import schedule from './schedule'
 import podcast from './podcast'
 import article from './article'
+import youtubevideo from './youtubevideo'
 
-export const schemaTypes = [hero, sermon, gallery, nugget, schedule, podcast, article]
+export const schemaTypes = [hero, sermon, gallery, nugget, schedule, podcast, article, youtubevideo]
